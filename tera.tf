688e9a7d96d949d3ed20ca24fad62f94c9afe2f4
jagadamma <96439002+jagadamma@users.noreply.github.com>
2022-12-14 01:39:38 +0530

    Update tera.tf

--- tera.tf
+++ tera.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region  = "us-west-2"
   access_key = "AKIAQMVASEQFZV6YMC5T"
-  secret_key = "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"
+  secret_key = "6aRA/EnhMzY+zxsNH8Q2J/d9ZZkTjz97Rtrysadh"
 }
 
 resource "aws_instance" "myserver" {
